afd28deb31e50dad62ad0ece631d02931cf3a9ee
kooksee <kooksee@163.com>
2019-12-31 17:46:23 +0800

    fix: add interface

--- dialect.go
+++ dialect.go
@@ -1,6 +1,8 @@
 package schema
 
-import "strings"
+import (
+	"strings"
+)
 
 // query defines dialect query types.
 type query int
@@ -45,4 +47,114 @@ var driverDialect map[string]*dialect = map[string]*dialect{
 // pack a string, normalising its whitespace.
 func pack(s string) string {
 	return strings.Join(strings.Fields(s), " ")
+}
+
+// Dialect interface contains behaviors that differ across SQL database
+type Dialect interface {
+	// GetName get dialect's name
+	GetName() string
+
+	// SetDB set db for dialect
+	//SetDB(db SQLCommon)
+
+	// BindVar return the placeholder for actual values in SQL statements, in many dbs it is "?", Postgres using $1
+	BindVar(i int) string
+	// Quote quotes field name to avoid SQL parsing exceptions by using a reserved word as a field name
+	Quote(key string) string
+	// DataTypeOf return data's sql type
+	//DataTypeOf(field *StructField) string
+
+	// HasIndex check has index or not
+	HasIndex(tableName string, indexName string) bool
+	// HasForeignKey check has foreign key or not
+	HasForeignKey(tableName string, foreignKeyName string) bool
+	// RemoveIndex remove index
+	RemoveIndex(tableName string, indexName string) error
+	// HasTable check has table or not
+	HasTable(tableName string) bool
+	// HasColumn check has column or not
+	HasColumn(tableName string, columnName string) bool
+	// ModifyColumn modify column's type
+	ModifyColumn(tableName string, columnName string, typ string) error
+
+	// LimitAndOffsetSQL return generated SQL with Limit and Offset, as mssql has special case
+	LimitAndOffsetSQL(limit, offset interface{}) string
+	// SelectFromDummyTable return select values, for most dbs, `SELECT values` just works, mysql needs `SELECT value FROM DUAL`
+	SelectFromDummyTable() string
+	// LastInsertIdReturningSuffix most dbs support LastInsertId, but postgres needs to use `RETURNING`
+	LastInsertIDReturningSuffix(tableName, columnName string) string
+	// DefaultValueStr
+	DefaultValueStr() string
+
+	// BuildKeyName returns a valid key name (foreign key, index key) for the given table, field and reference
+	BuildKeyName(kind, tableName string, fields ...string) string
+
+	// NormalizeIndexAndColumn returns valid index name and column name depending on each dialect
+	NormalizeIndexAndColumn(indexName, columnName string) (string, string)
+
+	// CurrentDatabase return current database name
+	CurrentDatabase() string
+}
+
+// ParseFieldStructForDialect get field's sql data type
+//var ParseFieldStructForDialect = func(field *StructField, dialect Dialect) (fieldValue reflect.Value, sqlType string, size int, additionalType string) {
+//	// Get redirected field type
+//	var (
+//		reflectType = field.Struct.Type
+//		dataType, _ = field.TagSettingsGet("TYPE")
+//	)
+//
+//	for reflectType.Kind() == reflect.Ptr {
+//		reflectType = reflectType.Elem()
+//	}
+//
+//	// Get redirected field value
+//	fieldValue = reflect.Indirect(reflect.New(reflectType))
+//
+//	if gormDataType, ok := fieldValue.Interface().(interface {
+//		GormDataType(Dialect) string
+//	}); ok {
+//		dataType = gormDataType.GormDataType(dialect)
+//	}
+//
+//	// Get scanner's real value
+//	if dataType == "" {
+//		var getScannerValue func(reflect.Value)
+//		getScannerValue = func(value reflect.Value) {
+//			fieldValue = value
+//			if _, isScanner := reflect.New(fieldValue.Type()).Interface().(sql.Scanner); isScanner && fieldValue.Kind() == reflect.Struct {
+//				getScannerValue(fieldValue.Field(0))
+//			}
+//		}
+//		getScannerValue(fieldValue)
+//	}
+//
+//	// Default Size
+//	if num, ok := field.TagSettingsGet("SIZE"); ok {
+//		size, _ = strconv.Atoi(num)
+//	} else {
+//		size = 255
+//	}
+//
+//	// Default type from tag setting
+//	notNull, _ := field.TagSettingsGet("NOT NULL")
+//	unique, _ := field.TagSettingsGet("UNIQUE")
+//	additionalType = notNull + " " + unique
+//	if value, ok := field.TagSettingsGet("DEFAULT"); ok {
+//		additionalType = additionalType + " DEFAULT " + value
+//	}
+//
+//	if value, ok := field.TagSettingsGet("COMMENT"); ok {
+//		additionalType = additionalType + " COMMENT " + value
+//	}
+//
+//	return fieldValue, dataType, size, strings.TrimSpace(additionalType)
+//}
+
+func currentDatabaseAndTable(dialect Dialect, tableName string) (string, string) {
+	if strings.Contains(tableName, ".") {
+		splitStrings := strings.SplitN(tableName, ".", 2)
+		return splitStrings[0], splitStrings[1]
+	}
+	return dialect.CurrentDatabase(), tableName
 }
--- dialect_sqlite.go
+++ dialect_sqlite.go
@@ -1,5 +1,12 @@
 package schema
 
+import (
+	"fmt"
+	"reflect"
+	"strings"
+	"time"
+)
+
 var sqlite = dialect{
 	queries: [3]string{
 		// columnTypes query.
@@ -21,4 +28,99 @@ var sqlite = dialect{
 				type = 'view'
 		`),
 	},
+}
+
+type sqlite3 struct {
+	commonDialect
+}
+
+func (sqlite3) GetName() string {
+	return "sqlite3"
+}
+
+// Get Data Type for Sqlite Dialect
+func (s *sqlite3) DataTypeOf(field *StructField) string {
+	var dataValue, sqlType, size, additionalType = ParseFieldStructForDialect(field, s)
+
+	if sqlType == "" {
+		switch dataValue.Kind() {
+		case reflect.Bool:
+			sqlType = "bool"
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uintptr:
+			if s.fieldCanAutoIncrement(field) {
+				field.TagSettingsSet("AUTO_INCREMENT", "AUTO_INCREMENT")
+				sqlType = "integer primary key autoincrement"
+			} else {
+				sqlType = "integer"
+			}
+		case reflect.Int64, reflect.Uint64:
+			if s.fieldCanAutoIncrement(field) {
+				field.TagSettingsSet("AUTO_INCREMENT", "AUTO_INCREMENT")
+				sqlType = "integer primary key autoincrement"
+			} else {
+				sqlType = "bigint"
+			}
+		case reflect.Float32, reflect.Float64:
+			sqlType = "real"
+		case reflect.String:
+			if size > 0 && size < 65532 {
+				sqlType = fmt.Sprintf("varchar(%d)", size)
+			} else {
+				sqlType = "text"
+			}
+		case reflect.Struct:
+			if _, ok := dataValue.Interface().(time.Time); ok {
+				sqlType = "datetime"
+			}
+		default:
+			if IsByteArrayOrSlice(dataValue) {
+				sqlType = "blob"
+			}
+		}
+	}
+
+	if sqlType == "" {
+		panic(fmt.Sprintf("invalid sql type %s (%s) for sqlite3", dataValue.Type().Name(), dataValue.Kind().String()))
+	}
+
+	if strings.TrimSpace(additionalType) == "" {
+		return sqlType
+	}
+	return fmt.Sprintf("%v %v", sqlType, additionalType)
+}
+
+func (s sqlite3) HasIndex(tableName string, indexName string) bool {
+	var count int
+	s.db.QueryRow(fmt.Sprintf("SELECT count(*) FROM sqlite_master WHERE tbl_name = ? AND sql LIKE '%%INDEX %v ON%%'", indexName), tableName).Scan(&count)
+	return count > 0
+}
+
+func (s sqlite3) HasTable(tableName string) bool {
+	var count int
+	s.db.QueryRow("SELECT count(*) FROM sqlite_master WHERE type='table' AND name=?", tableName).Scan(&count)
+	return count > 0
+}
+
+func (s sqlite3) HasColumn(tableName string, columnName string) bool {
+	var count int
+	s.db.QueryRow(fmt.Sprintf("SELECT count(*) FROM sqlite_master WHERE tbl_name = ? AND (sql LIKE '%%\"%v\" %%' OR sql LIKE '%%%v %%');\n", columnName, columnName), tableName).Scan(&count)
+	return count > 0
+}
+
+func (s sqlite3) CurrentDatabase() (name string) {
+	var (
+		ifaces   = make([]interface{}, 3)
+		pointers = make([]*string, 3)
+		i        int
+	)
+	for i = 0; i < 3; i++ {
+		ifaces[i] = &pointers[i]
+	}
+	if err := s.db.QueryRow("PRAGMA database_list").Scan(ifaces...); err != nil {
+		return
+	}
+	if pointers[1] != nil {
+		name = *pointers[1]
+	}
+	return
 }
